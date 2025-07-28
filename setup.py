e24fa3d88b29a905099aaf6c0e655faf031a359c
mukesh1996-ds <mks.mukesh1996@gmail.com>
2025-07-29 00:01:18 +0530

    notebook updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Chat With Codebase',
+    version='0.0.0',
+    author="Mukesh",
+    author_email="mks.mukesh1996@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
